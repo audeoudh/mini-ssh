d6501c5c42123adbac695b3a46dc80559e557565
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2017-12-21 19:37:35 +0100

    Login selection from the CLI (`-l` flag)

--- openssh_compatible.py
+++ openssh_compatible.py
@@ -12,28 +12,30 @@ def cli(args=None):
 
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
+        "-l", metavar="login_name",
+        help="Specifies the user to log in as on the remote machine")
+    argparser.add_argument(
         "destination", metavar="[user@]hostname",
         help="The remote machine to which ssh should connect, and the user "
              "name to use while performing authentication")
     options = argparser.parse_args(args)
 
     # Parse the destination
-    login_name = None
     try:
         # Erase login_name if already defined: this form is preferred
-        login_name, hostname = options.destination.rsplit("@", 1)
+        options.login_name, hostname = options.destination.rsplit("@", 1)
     except ValueError:
         # No login_name. Ok.
         hostname = options.destination
 
     # Check the username
-    if login_name == "":
+    if options.login_name == "":
         argparser.print_help(file=sys.stderr)
         exit(255)
-    elif login_name is None:
-        login_name = os.environ['USER']
+    elif options.login_name is None:
+        options.login_name = os.environ['USER']
 
-    main(login_name, hostname, 22)
+    main(options.login_name, hostname, 22)
 
 
 def main(user_name, server_name, port):
