8405e7456607dad552f128d232f8825451e82f47
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2017-12-21 19:39:48 +0100

    Verbosity selection from the CLI (`-v` flag)

--- openssh_compatible.py
+++ openssh_compatible.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 import os
 
@@ -12,6 +13,12 @@ def cli(args=None):
 
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
+        "-v", dest="verbose", action="count", default=0,
+        help="Verbose mode.  Causes ssh to print debugging messages about its "
+             "progress.  This is helpful in debugging connection, "
+             "authentication, and configuration problems.  Multiple -v "
+             "options increase the verbosity.  The maximum is 2.")
+    argparser.add_argument(
         "-p", metavar="port", default=22,
         help="Port to connect to on the remote host")
     argparser.add_argument(
@@ -22,6 +29,13 @@ def cli(args=None):
         help="The remote machine to which ssh should connect, and the user "
              "name to use while performing authentication")
     options = argparser.parse_args(args)
+
+    # Configure verbosity of logger
+    try:
+        log_level = (logging.WARNING, logging.INFO, logging.DEBUG)[options.verbose]
+    except IndexError:
+        log_level = logging.DEBUG  # Maximum verbosity
+    logging.basicConfig(format="[%(levelname)s] %(message)s", level=log_level)
 
     # Check the port
     if not (0x0000 < options.port <= 0xFFFF):
