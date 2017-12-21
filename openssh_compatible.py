3e1c2597c458acaf095d7efee36b83c99fb6baea
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2017-12-21 19:39:09 +0100

    Port selection from the CLI (`-p` flag)

--- openssh_compatible.py
+++ openssh_compatible.py
@@ -12,6 +12,9 @@ def cli(args=None):
 
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
+        "-p", metavar="port", default=22,
+        help="Port to connect to on the remote host")
+    argparser.add_argument(
         "-l", metavar="login_name",
         help="Specifies the user to log in as on the remote machine")
     argparser.add_argument(
@@ -19,6 +22,11 @@ def cli(args=None):
         help="The remote machine to which ssh should connect, and the user "
              "name to use while performing authentication")
     options = argparser.parse_args(args)
+
+    # Check the port
+    if not (0x0000 < options.port <= 0xFFFF):
+        print("Bad port '%d'" % options.port, file=sys.stderr)
+        exit(255)
 
     # Parse the destination
     try:
@@ -35,7 +43,7 @@ def cli(args=None):
     elif options.login_name is None:
         options.login_name = os.environ['USER']
 
-    main(options.login_name, hostname, 22)
+    main(options.login_name, hostname, options.port)
 
 
 def main(user_name, server_name, port):
